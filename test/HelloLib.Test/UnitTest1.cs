851461b8067d6dbe2a25f12fefc14d40674fe2aa
PatinyaNan <33483603+PatinyaNan@users.noreply.github.com>
2017-11-13 11:25:06 +0700

    Update UnitTest1.cs

--- test/HelloLib.Test/UnitTest1.cs
+++ test/HelloLib.Test/UnitTest1.cs
@@ -9,9 +9,9 @@ namespace HelloLib.Test
         [TestMethod]
         public void TestMethod1()
         {
-            string expected = "Hi, TetMethod1";
+            string expected = "Hi, TestMethod1";
             string name = "TestMethod1";
-            string actual = "Hi, " + name;
+            string actual = null;
 
             Assert.AreEqual(expected, actual);
         }
